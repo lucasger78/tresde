6034d60e74f8771e774e626bc6f78e29c339f935
Lucas Conti <lucasger78@hotmail.com>
2023-11-09 17:23:52 -0300

    Actualización final antes de subir

--- js/slider-materiales.js
+++ js/slider-materiales.js
@@ -16,23 +16,57 @@
 //         }
 //     });
 // });
+
+
+///////////// ACTUAL ///////////////
+// const prevBtn = document.querySelector('.prev');
+// const nextBtn = document.querySelector('.next');
+// const slides = document.querySelector('.slides');
+
+// let counter = 0;
+// const slideWidth = document.querySelector('.slide').clientWidth;
+
+// nextBtn.addEventListener('click', () => {
+//     if (counter < 8) {
+//         counter++;
+//         slides.style.transform = `translateX(${-counter * slideWidth}px)`;
+//     }
+// });
+
+// prevBtn.addEventListener('click', () => {
+//     if (counter > 0) {
+//         counter--;
+//         slides.style.transform = `translateX(${-counter * slideWidth}px)`;
+//     }
+// });
+
+
+///////////// PRUEBA ////////////
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
 const slides = document.querySelector('.slides');
 
-let counter = 0;
-const slideWidth = document.querySelector('.slide').clientWidth;
-
-nextBtn.addEventListener('click', () => {
-    if (counter < 8) {
-        counter++;
-        slides.style.transform = `translateX(${-counter * slideWidth}px)`;
-    }
-});
+let slideIndex = 0;
 
 prevBtn.addEventListener('click', () => {
-    if (counter > 0) {
-        counter--;
-        slides.style.transform = `translateX(${-counter * slideWidth}px)`;
-    }
+  if (slideIndex > 0) {
+    slideIndex--;
+  } else {
+    slideIndex = document.querySelectorAll('.slide').length - 1;
+  }
+  updateSlidePosition();
 });
+
+nextBtn.addEventListener('click', () => {
+  if (slideIndex < document.querySelectorAll('.slide').length - 1) {
+    slideIndex++;
+  } else {
+    slideIndex = 0;
+  }
+  updateSlidePosition();
+});
+
+function updateSlidePosition() {
+  const slideWidth = document.querySelector('.slide').offsetWidth;
+  slides.style.transform = `translateX(${-slideIndex * slideWidth}px)`;
+}
